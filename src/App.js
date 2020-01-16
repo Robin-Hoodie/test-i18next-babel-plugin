6ad82c4128b98e449ccd99baa8ce9428eafa745a
RobinHoodie <rhellemans@laterpay.net>
2020-01-16 16:35:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initReactI18next, Trans } from "react-i18next";
+import i18next from "i18next";
+import translations from "./translations/en-US/translation.json";
+
+i18next
+  .use(initReactI18next)
+  .init({
+    resources: {},
+    lng: "nl",
+    keySeparator: false,
+    nsSeparator: false,
+    interpolation: {
+      escapeValue: false
+    }
+  });
+i18next.addResourceBundle("en-US", "translation", translations);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Trans i18nKey="hello">
+          Hello <h1>world</h1> it is I!
+        </Trans>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
